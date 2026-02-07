459fa874afb84dbe2090daeb0618440be06396db
SOURABH KUMAR <140057299+sourabhmehta-coder@users.noreply.github.com>
2026-02-07 09:49:09 +0530

    Create DivisibleBy5.java

---
+++ DivisibleBy5.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Q3 {
+//Check if a number is even or odd.
+	public static void main(String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		
+		System.out.print("Enter a number: " );
+		int n = sc.nextInt();
+		
+		if(n%5==0) {
+			System.out.print(n+" is divisible By 5");
+		}else{
+			System.out.print(n+" is not divisible by 5" );
+		}
+		
+
+	}
+
+}
